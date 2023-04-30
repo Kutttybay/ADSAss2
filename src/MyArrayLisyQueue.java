252fc814f4345e472b38844d5c5901efef89879f
kutttybay <kutttybay@gmail.com>
2023-04-30 21:32:11 +0600

    4 Commit Ass3

--- src/MyArrayLisyQueue.java
+++ src/MyArrayLisyQueue.java
@@ -1,14 +1,23 @@
 public class MyArrayLisyQueue<T> {
     MyArrayList myArrayList = new MyArrayList<T>();
 
+    /*
+    * @return true if queue is empty, false otherwise
+    */
     public boolean isEmpty(){
         return this.size() == 0;
     }
 
+    /*
+    * @return number of elements in queue
+    */
     public int size(){
         return myArrayList.size();
     }
 
+    /*
+    * Check throw Exception in the list
+    */
     public void isEmptyThrowException() throws NoSuchFieldError {
         if (isEmpty()){
             throw new NoSuchFieldError();
