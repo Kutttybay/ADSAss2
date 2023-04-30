661961514a2740fa90be318581b890f1f2515980
kutttybay <kutttybay@gmail.com>
2023-04-30 21:15:45 +0600

    2 Commit Ass3

--- src/MyArrayLisyQueue.java
+++ src/MyArrayLisyQueue.java
@@ -1,3 +1,14 @@
 public class MyArrayLisyQueue<T> {
     MyArrayList myArrayList = new MyArrayList<T>();
+
+    /*
+    * Add element to the back of queue
+    * @param element the element added to the back of queue
+    */
+    public void enqueue(T element){
+        myArrayList.add(element);
+    }
+
+
+
 }
