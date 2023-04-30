121a627b95e6d75f92152d9b3d3ca9783059197f
kutttybay <kutttybay@gmail.com>
2023-04-30 22:06:29 +0600

    8 Commit Ass3

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,4 +1,33 @@
+import java.util.EmptyStackException;
+
 public class MyArrayListStack<T> {
     MyArrayList myArrayList = new MyArrayList<T>();
+
+    public boolean isEmpty(){
+        return this.size() == 0;
+    }
+
+    /*
+    * @return thw number of elements in stack
+    */
+    public int size(){
+        return myArrayList.size();
+    }
+
+    /*
+    * Check throw exceptions in the list
+    */
+    public void isEmptyThrowException(){
+        if (isEmpty()){
+            throw new EmptyStackException();
+        }
+    }
+
+    /*
+    * @param element The element will be add
+    */
+    public void push(T element){
+        myArrayList.add(element, 0);
+    }
 
 }
